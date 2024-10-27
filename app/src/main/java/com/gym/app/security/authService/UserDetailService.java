146101548f28eff3c1dbd7d277a695fb64d41e79
Dimitris Chatzopoulos <xatzic8@gmail.com>
2024-10-27 12:52:07 +0200

    connect between apps

--- app/src/main/java/com/gym/app/security/authService/UserDetailService.java
+++
@@ -1,7 +0,0 @@
-package com.gym.app.security.authService;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserDetailService {
-    UserDetails loadUserByUsername(String email);
-}
